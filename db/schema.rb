ed0657e3fa17887f13f39d0b11d700272805605a
Shawn <119657617+S-higss@users.noreply.github.com>
2024-07-18 07:24:17 +0000

    [Do]rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_18_065023) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_18_072323) do
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
